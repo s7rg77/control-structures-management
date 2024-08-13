eea871b58ee78b760c698483a7309b456f434eb7
s7rg77 <sxrgxx@gmail.com>
2024-08-13 17:51:00 +0200

    01

--- index.java
+++ index.java
@@ -9,24 +9,24 @@ public class CalculatePower {
         try {
 
             // Check if base is negative
-            if (base &lt; 0) {
+            if (base < 0) {
                 throw new Exception("The data must be 0 or greater than 0");
             }
 
             // Check if exponent is negative
-            if (exponent &lt; 0) {
+            if (exponent < 0) {
                 throw new Exception("The data must be 0 or greater than 0");
             }
 
             // Calculate power using a for loop
-            for (int exp = 0; exp &lt; exponent; exp++) {
+            for (int exp = 0; exp < exponent; exp++) {
                 result = base * result;
             }
             System.out.println("The result of " + base + " raised to " + exponent + " is: " + result);
             result = 1;
 
             // Calculate power using a while loop
-            while (exponent &gt; 0) {
+            while (exponent > 0) {
                 result = result * base;
                 exponent--;
             }
